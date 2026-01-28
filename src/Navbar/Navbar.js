1b01b2c8af8ca4ae43821a70deb5343184c6f984
react-tech <rajesharjun786@gmail.com>
2026-01-28 18:17:02 +0530

    footer added done

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -19,73 +19,77 @@ const Navbar = () => {
           </Navbar.Brand>
         </Container>
       </Navbar> */}
-      <nav className={ `shadow-xl sticky top-[10vh] w-full z-50 navbar navbar-expand-lg p-0
-      ${theme === 'dark' ? 'bg-gray-800 text-white' : 'bg-slate-100 text-black'}` }>
-        <div className=' container w-full me-auto px-3 py-0 flex items-center justify-end'>
+      <nav className={ `shadow-xl top-[0] sticky z-[100] border border-slate-200/50 glass-nav
+      ${theme === 'dark' ? 'text-white' : 'text-black'}` }>
+        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
 
-          {/* Logo */ }
-          <div className='w-40'>
-            <Link to="/">
-              <img src={ require('../assets/images/logoipsum-354.png') } alt="cybercity-brand"
-                className='w-full h-auto' />
-            </Link>
-            <form>
+          <div className=' flex justify-between items-center h-24'>
+            {/* Logo */ }
+            <div className=' flex items-center gap-2'>
+              <Link to="/" className=' flex items-center group gap-2'>
+                <img src={ require('../assets/images/react_brand.webp') } alt="react-allin-one-logo"
+                  className='w-auto object-contain h-20 group-hover:scale-105 transition-transform' />
+              </Link>
+              <form>
 
-            </form>
-          </div>
+              </form>
+            </div>
 
-          {/* Mobile Toggle */ }
-          <div className='md:hidden'>
-            <button onClick={ () => setIsOpen(!isOpen) } className='text-2xl focus:outline-none'>
-              { isOpen ? <i className="bi bi-x"></i> : <i className="bi bi-list"></i> }
-            </button>
-          </div>
+            {/* Mobile Toggle */ }
+            <div className='md:hidden'>
+              <button onClick={ () => setIsOpen(!isOpen) } className='text-2xl focus:outline-none'>
+                { isOpen ? <i className="bi bi-x"></i> : <i className="bi bi-list"></i> }
+              </button>
+            </div>
 
-          {/* Navigation */ }
-          <div className={ `absolute md:static top-16 left-0 md:w-auto md:bg-transparent 
+            {/* Navigation */ }
+            <div className={ `absolute md:static top-16 left-0 md:w-auto md:bg-transparent 
         transition-all duration-300 ease-in z-4 ms-auto
                         ${isOpen ? 'flex' : 'hidden'} lg:flex md:flex w-full` }>
-            <ul className='md:flex md:items-center space-y-0 md:space-y-0
-           mx-auto md:space-x-0 p-0 md:p-0 me-2 sm:bg-white lg:bg-transparent'>
-              { [
-                { to: '/home', label: 'Home' },
-                { to: '/common', label: 'CommonPage' },
-                { to: '/apicalls', label: 'APICalls' },
-                { to: '/customhooks', label: 'CustomHooks' },
-                { to: '/props', label: 'Props' },
-                { to: '/formik', label: 'Formik' },
-                { to: '/reacthooks', label: 'ReactHooks' },
-                { to: '/scroll', label: 'InfiniteScroll' },
-                { to: '/portal', label: 'React Portals' },
-                { to: '/todolist', label: 'HOC' }
-              ].map((item) => (
-                <li key={ item.to }>
-                  <NavLink
-                    to={ item.to }
-                    className={ ({ isActive }) =>
-                      `block px-2 py-3 rounded-0 transition font-medium text-sm text-slate-800
-                  ${isActive ? 'bg-green-700 text-white' : 'hover:text-green-700'}`
-                    }
-                  >
-                    { item.label }
-                  </NavLink>
-                </li>
-              )) }
-            </ul>
+              <ul className='md:flex md:items-center space-y-0 md:space-y-0
+           mx-auto md:space-x-0 p-0 md:p-0 me-2 sm:bg-white lg:bg-transparent gap-3'>
+                { [
+                  { to: '/home', label: 'Home' },
+                  { to: '/common', label: 'CommonPage' },
+                  { to: '/apicalls', label: 'APICalls' },
+                  { to: '/customhooks', label: 'CustomHooks' },
+                  { to: '/props', label: 'Props' },
+                  { to: '/formik', label: 'Formik' },
+                  { to: '/reacthooks', label: 'ReactHooks' },
+                  { to: '/scroll', label: 'InfiniteScroll' },
+                  { to: '/portal', label: 'React Portals' },
+                  { to: '/todolist', label: 'HOC' }
+                ].map((item) => (
+                  <li key={ item.to }>
+                    <NavLink
+                      to={ item.to }
+                      className={ ({ isActive }) =>
+                        `text-xs text-slate-600 font-semibold uppercase hover:text-blue-600 
+                      transition-colors
+                  ${isActive ? 'bg-green-600 text-sm text-white px-2.5 py-2.5 hover:bg-blue-800 rounded-xl font-bold' : ''}`
+                      }
+                    >
+                      { item.label }
+                    </NavLink>
+                  </li>
+                )) }
+              </ul>
 
-            {/* Theme Toggle Button */ }
-            <ul className='mt-2 md:mt-0 md:ml-4 flex items-center'>
-              <button
-                onClick={ () => setTheme(theme === 'light' ? 'dark' : 'light') }
-                className={ `text-sm px-3 py-3 text-xl  ${theme === 'light'
-                  ? 'text-white bg-sky-600 hover:bg-gray-200'
-                  : ' bg-orange-400 border-slate-700 text-white  hover:bg-orange-700'
-                  }` }
-              >
-                { theme === 'light' ? <i class="bi bi-brightness-low"></i> : <i class="bi bi-moon"></i> }
-              </button>
-            </ul>
+              {/* Theme Toggle Button */ }
+              <ul className='mt-2 md:mt-0 md:ml-4 flex items-center'>
+                <button
+                  onClick={ () => setTheme(theme === 'light' ? 'dark' : 'light') }
+                  className={ `text-xl ${theme === 'light'
+                    ? 'text-white bg-slate-900 px-3 py-2 rounded-xl hover:bg-green-600'
+                    : 'hover:bg-green-600'
+                    }` }
+                >
+                  { theme === 'light' ? <i class="bi bi-brightness-low"></i> : <i class="bi bi-moon"></i> }
+                </button>
+              </ul>
+            </div>
           </div>
+
         </div>
       </nav>
     </>
--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -1,31 +1,68 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
 
     return (
 
-        <section className='info-footer-bg pb-5 pt-5 text-white'>
-            <Container>
-                <Row>
-                    <Col xl={ 3 } sm={ 6 }>
-                        lfvjf
-                    </Col>
-                    <Col sm={ 6 } xl={ 2 } md={ 3 }>
-                        Company
-                    </Col>
-                    <Col sm={ 6 } xl={ 2 } md={ 3 }>
-                        pages
-                    </Col>
-                    <Col sm={ 6 } xl={ 2 } md={ 3 }>
-                        Information
-                    </Col>
-                    <Col sm={ 12 } md={ 3 }>
-                        Connect withus
-                    </Col>
-                </Row>
-            </Container>
-        </section>
+        <footer className='bg-slate-800 border-t border-slate-900 pt-8 pb-8 mt-auto text-slate-400'>
+            <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+
+                <div className='flex items-center justify-center gap-4 mb-4 relative '>
+
+                    <span className='w-12 h-[1px] bg-gray-400'></span>
+                    <h2 className='text-white uppercase text-3xl mb-0 
+                        font-semibold tracking-widest'>get in touch</h2>
+                    <span className='w-12 h-[1px] bg-gray-400'></span>
+
+                </div>
+
+                {/* quick-links */ }
+
+                <div className='flex items-center justify-center'>
+                    {/* links-heading */ }
+                    <h3 className=' uppercase text-xl text-slate-300 tracking-wider mb-3'>quick links</h3>
+                    {/* end */ }
+
+                </div>
+
+                <div className='max-w-3xl mx-auto'>
+                    <div className=' flex flex-wrap justify-center gap-3 gap-3'>
+                        { [
+                            'Home',
+                            'Common Page',
+                            'API Calls',
+                            'Custom Hooks',
+                            'Props',
+                            'FormiK',
+                            'React Hooks',
+                            'InfinitiScroll',
+                            'React Portals',
+                            'HOC'
+                        ].map((item, index) => (
+                            <button key={ index }
+                                className='px-6 py-2 rounded-xl bg-slate-50
+                            text-gray-700 text-sm uppercase hover:bg-blue-700 transition-colors'>
+                                { item }
+                            </button>
+                        )) }
+                    </div>
+                </div>
+
+                {/* end */ }
+
+
+                <div className='grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 sm:grid-cols-2 gap-6 mb-12'>
+                    <div className='col-span-1'>
+                        <div>
+                            <img src={ require('../../assets/images/react_brand.webp') }
+                                className='w-auto h-20' alt="react-brand" loading='lazy' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
     )
 }
 
